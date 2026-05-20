194cf8be0843d5395cd98957ed29cae4eb52a9fa
almogdadjabir <a.jabir@oxinus.io>
2026-05-20 17:30:43 +0400

    feat(l10n): add context.tr extension

---
+++ lib/common/localization/app_localizations_extension.dart
@@ -0,0 +1,6 @@
+import 'package:amana_pos/l10n/app_localizations.dart';
+import 'package:flutter/widgets.dart';
+
+extension AppLocalizationX on BuildContext {
+  AppLocalizations get tr => AppLocalizations.of(this)!;
+}
